f2f67ed7db483462a445282fd44cbe104bced157
maxi <maxiciarud98@gmail.com>
2025-03-24 23:36:11 -0300

    create tasklist endpoints working and db post get for tasklist

--- chat/src/routes/home.js
+++ chat/src/routes/home.js
@@ -1,5 +1,5 @@
 import express, { query } from 'express';
-import { getUserinfo } from '../database.js';
+import { getUserinfo, createTasklist, createTask, getTasklist } from '../database.js';
 import { encrypt, compare } from '../helpers/encrypt.js';
 import { createAccessToken } from '../helpers/jwt.js';
 import { authRequired } from '../middlewares/validateToken.js';
@@ -12,9 +12,48 @@ router.get('/main', authRequired, async (req, res) => {
 	return res.json({ user: user });
 });
 
-router.post('/try', async (req, res) => {
-	const user = { id: 2 };
+router.post('/createtasklist', async (req, res) => {
 	console.log(req.body);
+	let tasklist = {
+		userId: req.body.user,
+		name: req.body.name,
+		is_daily: req.body.is_daily,
+		is_weekly: req.body.is_weekly,
+		is_monthly: req.body.is_monthly,
+	};
+
+	let tasks = req.body;
+	delete tasks.user;
+	delete tasks.name;
+	delete tasks.is_daily;
+	delete tasks.is_weekly;
+	delete tasks.is_monthly;
+
+	const tasklistId = await createTasklist(
+		tasklist.userId,
+		tasklist.name,
+		tasklist.is_daily,
+		tasklist.is_weekly,
+		tasklist.is_monthly
+	);
+
+	await CreateTaskInDatabase(tasks, tasklistId);
+	return res.json({ response: tasklist });
+});
+
+router.get('/gettasklists', authRequired, async (req, res) => {
+	const tasklists = await getTasklist(req.id);
+	console.log(tasklist);
+
 	return res.json({ user: user });
 });
+
+const CreateTaskInDatabase = async (task, tasklistId) => {
+	let res = -1;
+	for (const key of Object.keys(task)) {
+		res = await createTask(task[key], tasklistId);
+	}
+	return res;
+};
+
 export default router;
